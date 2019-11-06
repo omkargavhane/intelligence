8d534fa90444302a94f33dd2bfdb1fd94e7d257d
omkar gavhane <41783462+omkargavhane@users.noreply.github.com>
2019-11-06 10:41:38 +0530

    Update dependencies.sh

--- CropAdvApp/dependencies.sh
+++ CropAdvApp/dependencies.sh
@@ -1,6 +1,6 @@
-/*selenium >=3.141.0
-wikipedia >=1.4.0
-googletrans >=2.4.0*/
+#selenium >=3.141.0
+#wikipedia >=1.4.0
+#googletrans >=2.4.0
 pip3 install selenium==3.141.0
 pip3 install wikipedia==1.4.0
 pip3 install googletrans==2.4.0
